9ca25b833e62d2f32931b62bcac9d6de916780ea
SimonGit2022 <simon.soma.benedek@blathy.info>
2024-01-10 08:01:32 +0100

    This, is a new version that fixed the errors with the shop.

--- Harc.cs
+++ Harc.cs
@@ -214,7 +214,7 @@ namespace Path_to_Argon___Beta_v._2._0
                     {
                         Console.Write($"Hány elixirt kérsz:");
                         elixirek_szama = int.Parse(Console.ReadLine());
-                        while (elixirek_szama> jatekos.GetPenz() / elixir_ar || elixirek_szama < jatekos.GetPenz() / elixir_ar)//Ha, nem add, meg jó értéket.
+                        while (elixirek_szama> (jatekos.GetPenz() / elixir_ar) || elixirek_szama < 0)//Ha, nem add, meg jó értéket.
                         {
                             Console.WriteLine("Ennyit, nem tudsz venni!");
                             Console.Write("Probáld, újra:");
